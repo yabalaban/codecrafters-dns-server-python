4ce4b60e8be8f3d4615fd3ecdf4317447cab67a0
Alexander Balaban <balaban.alexander+github@gmail.com>
2024-09-28 20:41:52 +0100

    uncommit code

--- app/main.py
+++ app/main.py
@@ -7,20 +7,21 @@ def main():
 
     # Uncomment this block to pass the first stage
     #
-    # udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    # udp_socket.bind(("127.0.0.1", 2053))
-    #
-    # while True:
-    #     try:
-    #         buf, source = udp_socket.recvfrom(512)
-    #
-    #         response = b""
-    #
-    #         udp_socket.sendto(response, source)
-    #     except Exception as e:
-    #         print(f"Error receiving data: {e}")
-    #         break
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp_socket.bind(("127.0.0.1", 2053))
+    
+    while True:
+        try:
+            buf, source = udp_socket.recvfrom(512)
+    
+            response = b""
+    
+            udp_socket.sendto(response, source)
+        except Exception as e:
+            print(f"Error receiving data: {e}")
+            break
 
 
 if __name__ == "__main__":
     main()
+    
