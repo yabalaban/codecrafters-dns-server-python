7bbfe951eca71d9c424201bfaf7f1e0bde059dd5
Alexander Balaban <balaban.alexander+github@gmail.com>
2024-09-28 23:02:51 +0100

    answer class

--- app/main.py
+++ app/main.py
@@ -2,27 +2,62 @@ from enum import Enum
 import socket
 
 
+def _32bit_get(data: bytearray, offset: int) -> int:
+    return (data[offset] << 24) + (data[offset + 1] << 16) + (data[offset + 2] << 8) + data[offset + 3]
+
+
+def _32bit_set(data: bytearray, offset: int, value: int):
+    data[offset] = (value & 0xFF000000) >> 24
+    data[offset + 1] = (value & 0x00FF0000) >> 16
+    data[offset + 2] = (value & 0x0000FF00) >> 8
+    data[offset + 3] = (value & 0x000000FF)
+
+
 def _16bit_get(data: bytearray, offset: int) -> int:
     return (data[offset] << 8) + data[offset + 1]
+
 
 def _16bit_set(data: bytearray, offset: int, value: int):
     data[offset] = (value & 0xFF00) >> 8
     data[offset + 1] = value & 0x00FF
 
+
 def _bit_get(data: bytearray, offset: int, shift: int) -> int:
     return (data[offset] & (0x01 << shift)) >> shift
+
 
 def _bit_set(data: bytearray, offset: int, shift: int, value: int):
     data[offset] = data[offset] & ~(1 << shift)
     if value:
         data[offset] = data[offset] | (1 << shift)
 
+
 def _bits_get(data: bytearray, offset: int, mask: int, shift: int) -> int:
     return (data[offset] & mask) >> shift
+
 
 def _bits_set(data: bytearray, offset: int, mask: int, shift: int, value: int):
     data[offset] = data[offset] & ~mask
     data[offset] = data[offset] | (value << shift)
+
+
+def _encode_labels(value) -> bytearray:
+    arr = bytearray(0)
+    for label in value.split('.'):
+        arr.append(len(label))
+        arr.extend(bytes(label, encoding='utf-8'))
+    arr.append(0x00)
+    return arr
+
+
+def _decode_labels(bytes: bytearray, offset: int) -> str:
+    s = ''
+    while bytes[offset] != 0x00: 
+        n = int(bytes[offset])
+        offset += 1
+        s += bytes[offset: offset + n].decode("utf-8")
+        offset += n 
+    return s
 
 
 class DNSHeader:
@@ -138,7 +173,10 @@ class DNSHeader:
         _16bit_set(self._payload, 10, value)
 
 
-class DNSQuestionType(Enum):
+class DNSRRType(Enum):
+    # TYPE fields are used in resource records.  Note that these types are a
+    # subset of QTYPEs.
+
     A = 1       # a host address                
     NS = 2      # an authoritative name server
     MD = 3      # a mail destination (Obsolete - use MX)
@@ -157,7 +195,10 @@ class DNSQuestionType(Enum):
     TXT = 16    # text strings
 
 
-class DNSQuestionClass(Enum):
+class DNSRRClass(Enum):
+    # CLASS fields appear in resource records.  The following CLASS mnemonics
+    # and values are defined:
+
     IN = 1 # the Internet
     CS = 2 # the CSNET class (Obsolete - used only for examples in some obsolete RFCs)
     CH = 3 # the CHAOS class
@@ -170,61 +211,115 @@ class DNSQuestion:
 
     def payload(self) -> bytearray:
         return bytearray(self._payload)
+    
+    @property
+    def domain_name(self) -> str:
+        return _decode_labels(self._payload, 0)
+    
+    @domain_name.setter
+    def domain_name(self, value: str) -> str:
+        self._payload[:-4] = _encode_labels(value)
+    
+    @property
+    def typ(self) -> DNSRRType:
+        return DNSRRType(_16bit_get(self._payload, len(self._payload) - 4))
+    
+    @typ.setter
+    def typ(self, ty: DNSRRType):
+        _16bit_set(self._payload, len(self._payload) - 4, ty.value)
+
+    @property
+    def cls(self) -> DNSRRClass:
+        return DNSRRClass(_16bit_get(self._payload, len(self._payload) - 2))
+    
+    @cls.setter
+    def cls(self, cls: DNSRRClass):
+        _16bit_set(self._payload, len(self._payload) - 2, cls.value)
+
+
+class DNSAnswer:
+    def __init__(self, payload: bytearray):
+        self._payload = payload 
+        self._label_len = 0
+
+    def payload(self) -> bytearray:
+        return bytearray(self._payload)
 
     def labels(self) -> bytearray:
         return self._payload[:-4]
     
     @property
-    def domain_name(self) -> str:
-        name = ''
-        offset = 0
-        while self._payload[offset] != 0x00: 
-            n = int(self._payload[offset])
-            offset += 1
-            name += self._payload[offset: offset + n].decode("utf-8")
-            offset += n 
-        return name
+    def name(self) -> str:
+        return _decode_labels(self._payload, 0)
     
-    @domain_name.setter
-    def domain_name(self, value: str) -> str:
-        ba = bytearray(0)
-        for label in value.split('.'):
-            ba.append(len(label))
-            ba.extend(bytes(label, encoding='utf-8'))
-        ba.append(0x00)
-        self._payload[:-4] = ba
+    @name.setter
+    def name(self, value: str):
+        bytes = _encode_labels(value)
+        self._payload[:self._label_len] = bytes
+        self._label_len = len(bytes)
     
     @property
-    def typ(self) -> DNSQuestionType:
-        return DNSQuestionType(_16bit_get(self._payload, len(self._payload) - 4))
+    def typ(self) -> DNSRRType:
+        return DNSRRType(_16bit_get(self._payload, self._label_len))
     
     @typ.setter
-    def typ(self, ty: DNSQuestionType):
-        _16bit_set(self._payload, len(self._payload) - 4, ty.value)
+    def typ(self, ty: DNSRRType):
+        _16bit_set(self._payload, self._label_len, ty.value)
 
     @property
-    def cls(self) -> DNSQuestionClass:
-        return DNSQuestionClass(_16bit_get(self._payload, len(self._payload) - 2))
+    def cls(self) -> DNSRRClass:
+        return DNSRRClass(_16bit_get(self._payload, self._label_len + 2))
     
     @cls.setter
-    def cls(self, cls: DNSQuestionClass):
-        _16bit_set(self._payload, len(self._payload) - 2, cls.value)
+    def cls(self, cls: DNSRRClass):
+        _16bit_set(self._payload, self._label_len + 2, cls.value)
 
+    @property
+    def ttl(self) -> int:
+        return _32bit_get(self._payload, self._label_len + 4)
+    
+    @ttl.setter
+    def ttl(self, value: int):
+        _32bit_set(self._payload, self._label_len + 4, value)
 
+    @property
+    def length(self) -> int:
+        return _16bit_get(self._payload, self._label_len + 8)
+    
+    @length.setter
+    def length(self, value: int):
+        _16bit_set(self._payload, self._label_len + 8, value)
+
+    @property
+    def data(self) -> bytearray:
+        return self._payload[self._label_len + 10:]
+    
+    @data.setter
+    def data(self, value: bytearray):
+        self.length = len(value)
+        self._payload[self._label_len + 10:] = value
+
+    
 class DNSMessage:
     def __init__(self, header: DNSHeader):
         self._header = header 
         self._questions = []
+        self._answers = []
 
     def payload(self) -> bytes:
         bytes = bytearray(0)
         bytes.extend(self._header.payload())
         [bytes.extend(q.payload()) for q in self._questions]
+        [bytes.extend(a.payload()) for a in self._answers]
         return bytes
 
     def add_question(self, question: DNSQuestion):
         self._header.qdcount += 1
         self._questions.append(question)
+
+    def add_answer(self, answer: DNSAnswer):
+        self._header.ancount += 1
+        self._answers.append(answer)
 
 
 def main():
@@ -244,10 +339,10 @@ def main():
 
             question = DNSQuestion(bytearray(4))
             question.domain_name = "codecrafters.io"
-            question.typ = DNSQuestionType.A
-            question.cls = DNSQuestionClass.IN
+            question.typ = DNSRRType.A
+            question.cls = DNSRRClass.IN
             message.add_question(question)
-            
+
             response = message.payload()
     
             udp_socket.sendto(response, source)
